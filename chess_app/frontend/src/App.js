f2f73f5a632a0da1640e2879e0823327cca7247e
BojanRobev <bojan.robev@gmail.com>
2023-09-21 02:13:27 +0200

    update routes

--- chess_app/frontend/src/App.js
+++ chess_app/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import CaptureComponent from "./pages/CapturePhotoPage";
-import ChessGame from "./components/ChessGame";
+import ChessPage from "./pages/ChessPage";
 import Test from "./components/Test";
 import HomePage from "./pages/HomePage";
 import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import EditBoardPage from "./pages/EditBoardPage";
+import AnalysingImagePage from "./pages/AnalysingImagePage";
 
 export default class App extends Component {
   constructor(props) {
@@ -17,8 +19,10 @@ export default class App extends Component {
       <Routes>
         <Route path="/*" element={<HomePage />} />
         <Route path="/test" element={<Test />} />
-        <Route path="/chess" element={<ChessGame />} />
+        <Route path="/chess" element={<ChessPage />} />
         <Route path="/capture" element={<CaptureComponent />} />
+        <Route path="/edit" element={<EditBoardPage />} />
+        <Route path="/loading" element={<AnalysingImagePage />} />
       </Routes>
     );
   }
